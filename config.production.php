b080b5d7c9118ba40313684b9855b93f80815c27
Shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2024-12-11 20:57:04 +0530

    Update config.production.php

--- config.production.php
+++ config.production.php
@@ -3,7 +3,7 @@
 return [
     'production' => true,
     'api_url'=>'https://vaaar-backend.onrender.com' ,
-    'baseUrl' => 'shubhammauryainfo.github.io/vaaarfoundatio'
+    'baseUrl' => 'https://shubhammauryainfo.github.io/vaaarfoundation'
    
    
 ];
